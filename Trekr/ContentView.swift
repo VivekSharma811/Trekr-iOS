8455655c66b6d7e0dbdaef3af592dfb9866bc9fa
VivekSharma811 <vs811861@gmail.com>
2021-05-15 20:11:49 +0530

    Locations class added

--- Trekr/ContentView.swift
+++ Trekr/ContentView.swift
@@ -8,21 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let location: Location
+    
     var body: some View {
-        VStack {
-            Image("highlands")
+        ScrollView {
+            Image(location.heroPicture)
                 .resizable()
                 .scaledToFit()
             
-            Text("The highlands")
+            Text(location.name)
                 .font(.largeTitle)
+                .bold()
+                .multilineTextAlignment(.center)
             
+            Text(location.country)
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(location.description)
+                .padding(.horizontal)
+            
+            Text("Did you know?")
+                .font(.title3)
+                .bold()
+                .padding(.top)
+            
+            Text(location.more)
+                .padding(.horizontal)
         }
+        .navigationTitle("Discover")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(location: Location.example)
     }
 }
