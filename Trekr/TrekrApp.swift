3f2a99808c6bcb7c6e61c4bde931b9bae9329a5e
VivekSharma811 <vs811861@gmail.com>
2021-05-15 17:56:11 +0530

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Vivek Sharma on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
