fbb87a8cbd3d26b525f45d6c3114c535f706373b
VivekSharma811 <vs811861@gmail.com>
2021-05-16 13:31:25 +0530

    Country Maps added to the Map

--- Trekr/WorldView.swift
+++ Trekr/WorldView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 
 struct WorldView: View {
     
+    @EnvironmentObject var locations: Locations
+    
     @State var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude:51.507222,
                                        longitude: -0.1275),
@@ -17,8 +19,19 @@ struct WorldView: View {
     )
     
     var body: some View {
-        Map(coordinateRegion: $region)
-            .navigationTitle("Locations")
+        Map(coordinateRegion: $region,
+            annotationItems: locations.places) {
+            location in
+            MapAnnotation(coordinate:
+                            CLLocationCoordinate2D(latitude: location.latitude, longitude: location.longitude)) {
+                Image(location.country)
+                    .resizable()
+                    .cornerRadius(10)
+                    .frame(width: 80, height: 40)
+                    .shadow(radius: 3)
+            }
+        }
+        .navigationTitle("Locations")
     }
 }
 
